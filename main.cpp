afccd8e3a59f0654384b5ed7fcb97c9b99642957
Sudarshan Sinha <sudarshan@Sudarshans-MacBook-Air.local>
2022-02-17 15:52:14 +0530

    second commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+int binarySearch(int arr[], int n, int key){
+    int s =0;
+    int e = n;
+    while (s<=e)
+    {
+        int mid = (s+e)/2;
+        if (arr[mid]==key)   
+        {
+            return mid;
+        }else if (arr[mid]<key)
+        {
+            s = mid+1;
+        }else{
+            e = mid-1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     #ifndef ONLINE_JUDGE
@@ -10,20 +29,16 @@ int main()
         freopen("output.txt", "w", stdout);
     #endif
 
-    
     int n;
-    int r = 0;
     cin>>n;
-    while (n>0)
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        int m = n%10;
-        n = (n-m)/10;
-        r = (r*10)+m;
+        cin>>arr[i];
     }
-    cout<<r;
+    int key;
+    cin>>key;
+    cout<<binarySearch(arr,n,key)+1;
     
-    
-    
-    //sudarshan
     return 0;
 }
